d8eb1e1d1655aeae45691dfc6e0fc4c5db1c2b2b
kamil2789 <kamil2789@wp.pl>
2024-05-24 19:36:38 +0200

    Add component enum

--- phoenix/src/components.rs
+++ phoenix/src/components.rs
@@ -1,3 +1,28 @@
+use self::{color::RGBA, geometry::Triangle, shaders::shader_program::ShaderProgram};
+
 pub mod color;
 pub mod geometry;
 pub mod shaders;
+
+
+pub enum Component {
+    Color(RGBA),
+    Geometry(Triangle),
+    ShaderProgram(ShaderProgram),
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::components::Component;
+
+    use std::mem;
+    const MEMORY_USAGE_FOR_COMPONENTS_ENUM: usize = 48;
+
+    #[test]
+    fn test_check_maximum_memory_usage_for_components_enum() {
+        assert_eq!(
+            MEMORY_USAGE_FOR_COMPONENTS_ENUM,
+            mem::size_of::<Component>()
+        );
+    }
+}
