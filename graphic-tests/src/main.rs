2ba9393f0ed91b2fefd72b60d69d1a2493d5c1c9
kamil2789 <kamil2789@wp.pl>
2024-11-21 21:45:23 +0100

    add clap and prototype use

--- graphic-tests/src/main.rs
+++ graphic-tests/src/main.rs
@@ -1,10 +1,17 @@
+use clap::Parser;
 use tests::run;
 
 mod image;
 mod tests;
 mod workspace;
+mod args_parser;
+
 
 fn main() {
+    let args = args_parser::Args::parse();
+
+    print!("{:?}", args.name);
+
     println!("Start graphic tests");
     run();
 }
